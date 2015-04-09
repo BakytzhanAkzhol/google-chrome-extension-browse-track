47154621bfc89702e40cc2b5bac4c95aab5f720f
Bakytzhan <bakytzhan.akzhol@ce.sdu.edu.kz>
2015-04-09 20:16:37 +0600

    Hello

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,15 @@
+ 'use strict';
+ function formatTime(seconds) {
+  //var hh = Math.floor(seconds / 3600);
+  var mm = Math.floor(seconds / 60) % 60,
+  ss = Math.floor(seconds) % 60;
+  return ((mm<10)?'0'+mm:mm)+":"+((ss<10)?'0'+ss:ss);
+}
 (function() {
-  'use strict';
-  var Stat, calc, tabChanged, updateBadge;
-
+  var Stat, calc, tabChanged, updateBadge,timer,res;
   chrome.runtime.onInstalled.addListener(function(details) {
     return console.log('previousVersion', details.previousVersion);
   });
-
   chrome.browserAction.setBadgeText({
     text: 'Hello'
   });
@@ -24,17 +28,19 @@
     Stat.cur = url;
     lst = Stat.data[url] || [];
     lst.push(new Date());
-    return Stat.data[url] = lst;
+    Stat.data[url]=lst;
+    return Stat.data[url];
   };
 
   calc = function(url) {
-    var i, lst, n, res, _i;
+    var i, lst, n, _i;
     lst = Stat.data[url];
     if (!lst) {
       return 0;
     }
     n = Math.floor(lst.length / 2);
     res = 0;
+    console.log(res);
     for (i = _i = 0; 0 <= n ? _i <= n : _i >= n; i = 0 <= n ? ++_i : --_i) {
       if (lst[2 * i + 1] && lst[2 * i]) {
         res += lst[2 * i + 1].getTime() - lst[2 * i].getTime();
@@ -51,10 +57,6 @@
       text: "" + formatTime(res / 1000)
     });
   };
-  var timer;
-  chrome.alarms.onAlarm.addListener(function( alarm ) {
-    console.log("Got an alarm!", alarm);
-  });
   chrome.tabs.onActivated.addListener(function(activeInfo) {
     //console.log("Select " + activeInfo.tabId + " ");
     Stat.curTabId = activeInfo.tabId;
@@ -63,39 +65,30 @@
         tabChanged(tab.url);
       }
       timer=window.setInterval(function() {
-       if (!Stat.curTabId) {
-        console.log("Interval is cleaner");
-        clearInterval(timer);
-        timer=null;
-        return;
-      }
-      return chrome.tabs.get(Stat.curTabId, function(tab) {
-        console.log(tab);
-        if (tab.url) {
-          return updateBadge(tab.url);
-        }
-      });
-      }, 250);
+        return chrome.tabs.get(Stat.curTabId, function(tab) {
+       //   console.log(tab);
+          if (tab.url) {
+             return updateBadge(tab.url);
+          }
+          });
+    }, 1000);
       return updateBadge(tab.url);
     });
-    chrome.tabs.onActiveChanged.addListener(function(activeInfo){
-      if(timer!=null){
-        console.log("Interval is cleaner");
-        clearInterval(timer);
-        timer=null;
-    }  
-  });
-    
   });
 
-
-  console.log('Browse Trucks is starting works');
-
+  chrome.tabs.onActiveChanged.addListener(function(){
+    if(timer!=null){
+      console.log("Interval is cleaner");
+      var n=''+Stat.curTabId;
+      clearInterval(timer);
+      timer=null;
+      
+      chrome.storage.sync.set({n:res}, function() {
+         chrome.storage.sync.get(n, function(data) {
+          var t=data;
+          console.log(t);
+        });
+      });}  
+    });
+ // console.log('Browse Trucks is starting works');
 }).call(this);
-
-function formatTime(seconds) {
-  var hh = Math.floor(seconds / 3600),
-    mm = Math.floor(seconds / 60) % 60,
-    ss = Math.floor(seconds) % 60;
-  return ((mm<10)?'0'+mm:mm)+":"+((ss<10)?'0'+ss:ss);
-}
